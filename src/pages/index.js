aa0ceb7cc6a7538fd2ddf7077f0d16c2fdd275f6
Maximilian Sellmer <sellmer@new-communication.de>
2017-11-12 17:38:27 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+const WideInput = styled.input`
+    border: 0;
+    border-bottom: 2px solid #3c4448;
+    width: 100%;
+    font-size: 30px;
+    line-height: 35px;
+    height: 70px;
+    text-align: center;
+    padding: 10px;
+    background: transparent;
+    color: #3c4448;
+
+    &:focus {
+        outline: 0;
+        color: #3c4448;
+    }
+    &::placeholder {
+        color: #3c4448;
+    }
+`
+
+const Wrapper = styled.div `
+    display: flex;
+    justify-content: center;
+`
+const BigText = styled.h1 `
+  font-size: 75px;
+  font-family: 'helvetica';
+`
+
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return(
+        <div>
+            <Wrapper>
+                <form>
+                    <WideInput type="text" placeholder="Input" value={this.state.value} onChange={this.handleChange} />
+                </form>
+            </Wrapper>
+            <Wrapper>
+                <BigText>{this.state.value}</BigText>
+            </Wrapper>
+        </div>
+    )
+  }
+}
+
+export default IndexPage
+
