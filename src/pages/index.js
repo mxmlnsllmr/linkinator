135e36a7dc6477d8296f94d864091f60fdae53a1
Maximilian Sellmer <sellmer@new-communication.de>
2017-11-14 12:19:04 +0100

    added sexy border-bottom for input

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,46 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import Link from 'gatsby-link'
 
 const WideInput = styled.input`
-    border: 0;
-    border-bottom: 2px solid #3c4448;
-    width: 100%;
-    font-size: 30px;
-    line-height: 35px;
-    height: 70px;
-    text-align: center;
-    padding: 10px;
-    background: transparent;
-    color: #3c4448;
-
-    &:focus {
-        outline: 0;
-        color: #3c4448;
-    }
-    &::placeholder {
-        color: #3c4448;
-    }
+  height: 60px;
+  width: 100%;
+  min-width: 100%;
+  padding: 0;
+  border-radius: 0;
+  line-height: 70px;
+  background-color: transparent;
+  color: #3c4448;
+  font-size: 30px;
+  border: none;
+  outline: none;
+  border-bottom: 3px solid #3c4448;
+  font-family: Roboto Slab, sans-serif;
 `
 
-const FormContainer = styled.form`
-    width: 100%;
+const HighlightInput = styled.span`
+  font-size: 30px;
+  line-height: 70px;
+  max-width: 100%;
+  border-top: 3px solid #E50064;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 0;
+  color: transparent;
+  user-select: none;
+  overflow: hidden;
+  font-family: Roboto Slab, sans-serif;
 `
-
 
 const Wrapper = styled.div `
     display: flex;
     justify-content: center;
+`
+
+const InputWrapper = styled.div`
+  width: 500px;
+  margin: 50px auto;
+  position: relative;
 `
 const BigText = styled.h1 `
   font-size: 75px;
@@ -46,19 +56,22 @@ class IndexPage extends Component {
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    const { value } = event.target;
+    this.setState({
+      value: value
+    });
   }
 
   render() {
+    const { value } = this.state;
     return(
         <div>
+          <InputWrapper>
+              <WideInput type="text" placeholder="Type in text" value={this.state.value} onChange={this.handleChange} />
+              <HighlightInput className='input-highlight'>{ value.replace(/ /g, "\u00a0") }</HighlightInput>
+          </InputWrapper>
             <Wrapper>
-                <FormContainer>
-                    <WideInput type="text" placeholder="Type in text" value={this.state.value} onChange={this.handleChange} />
-                </FormContainer>
-            </Wrapper>
-            <Wrapper>
-                <BigText>{this.state.value}</BigText>
+                <BigText>{value}</BigText>
             </Wrapper>
         </div>
     )
